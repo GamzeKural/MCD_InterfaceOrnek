cae900b52da5874640bd56d9135ce26d0cf6d081
GAMZE KURAL <gamzekural98@gmail.com>
2021-12-06 17:20:22 +0300

    Add project files.

---
+++ MCD_InterfaceOrnek/IYetenek1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_InterfaceOrnek
+{
+    interface IYetenek1
+    {
+        /*
+         * interface'in kendi erişim belirleyicisi neyse içindeki elemanların
+         * hepsi aynı erişime sahiptir.
+         * 
+         * bu nedenle interface içindeki elemanlara erişim belirleyicisi yazılmaz.
+         * 
+         * interface'lerin metotlarının gövdesi olmaz.
+         */
+
+        void X(); //gövdesi yok.
+
+        void Y();
+
+        void Z();
+
+    }
+}
